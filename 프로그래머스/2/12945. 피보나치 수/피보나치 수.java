fd8cc3286a0546c54de06b4b98bbae2c86731378
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-01-30 16:42:45 +0900

    [level 2] Title: 피보나치 수, Time: 4.16 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] F = new int[n+1];
+        
+        F[0] = 0;
+        F[1] = 1;
+        for (int i=2; i<=n; i++){
+            F[i] = (F[i-1] + F[i-2]) %1234567;
+        }
+        
+        answer = F[n];
+        return answer;
+    }
+}
